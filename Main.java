25c707585c97ab24688f9a1cf3202ca5ccbc0c60
MBY4444 <84253932+MBY4444@users.noreply.github.com>
2022-01-27 08:37:40 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+// Enter the length of password (int)
+
+import java.util.Scanner;
+
+public class Main
+{
+    static String generatePassword(int len){
+        String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        String output = "";
+        for (int i = 0; i < len; i++){
+            int random = (int)Math.round(Math.random() * (characters.length() - 1));
+            output += characters.charAt(random);
+        }
+        return output;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter length of characters: ");
+        int input = scan.nextInt();
+        String password = generatePassword(input);
+        System.out.println(password);
+    }
+}
